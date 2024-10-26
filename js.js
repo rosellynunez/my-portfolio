e9beaac0bf9f88d3fa85bc1455dd60d7a03f2482
Roselly Nuñez <roselly.nunezg@gmail.com>
2024-10-26 10:15:44 +0200

    subo todo antes q mi mac se muera

--- js.js
+++ js.js
@@ -4,43 +4,56 @@ const temaOscuro = () => {
     // Cambia el icono a sol 🌞 (bi-sun-fill)
     document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
 
-    document.querySelector("#intro").classList.remove("claro");
-    document.querySelector("#intro").classList.add("oscuro");
+    document.querySelector("#contact-me").classList.remove("claro");
+    document.querySelector("#contact-me").classList.add("oscuro");
+
+    document.querySelector(".rounded-block").classList.remove("claro");
+    document.querySelector(".rounded-block").classList.add("oscuro");
 
     // Cambia color de los h1, h2, h3
     document.querySelectorAll("h1, h2, h3").forEach((element) => {
     element.style.color = "white"; // contraste para el tema oscuro
     });
-}
+    
+    // Change color of #typed-output
+    document.querySelector("#typed-output").style.color = "white";
+};
 
 const temaClaro = () => {
      // Cambia el tema a claro
     document.querySelector("body").setAttribute("data-bs-theme", "light");
     // Cambia el icono a luna 🌛(bi-moon-fill)
     document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-fill");
- 
-    document.querySelector("#intro").classList.remove("oscuro");
-    document.querySelector("#intro").classList.add("claro");
+
+    document.querySelector("#contact-me").classList.remove("oscuro");
+    document.querySelector("#contact-me").classList.add("claro");
+
+    document.querySelector(".rounded-block").classList.remove("oscuro");
+    document.querySelector(".rounded-block").classList.add("claro");
 
     // Cambiar color de los h1, h2, h3
     document.querySelectorAll("h1, h2, h3").forEach((element) => {
     element.style.color = "black"; // contraste para el tema oscuro
     });
-}
+
+    // Change color of #typed-output
+    document.querySelector("#typed-output").style.color = "black";
+};
 
 const cambiarTema = () => {
     document.querySelector("body").getAttribute("data-bs-theme") === "light"?
     temaOscuro() : temaClaro();
-}
+};
 
 document.addEventListener('DOMContentLoaded', function() {
     var options = {
       strings: [
-        "to launch new websites 🚀",
-        "to drive conversions 🚀",
-        "to create amazing digital products 🚀"
+        "to build scalable design systems for future growth 🛠️",
+        "to design user interfaces that delight 🚀",
+        "to empower them with data-driven UX strategies 🌟",
+        "to craft user experiences that enhance engagement 📈"
       ],
-      typeSpeed: 50,
+      typeSpeed: 60,
       backSpeed: 25,
       backDelay: 2000,
       startDelay: 1000,
@@ -50,3 +63,28 @@ document.addEventListener('DOMContentLoaded', function() {
   
     var typed = new Typed("#typed-output", options);
   });
+
+  document.addEventListener("scroll", function () {
+    const navbar = document.querySelector(".navbar");
+    if (window.scrollY > 50) {  // Cambia el valor si quieres que el efecto ocurra antes o después
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  });
+
+  
+  function toggleMenu(toggler) {
+    toggler.classList.toggle("open");
+    
+    // Alternar entre la palabra "MENU" y la "X"
+    const menuText = toggler.querySelector('.navbar-toggler-text');
+    if (toggler.classList.contains("open")) {
+        menuText.style.display = 'none'; // Oculta "MENU" cuando está abierto
+    } else {
+        menuText.style.display = 'inline-block'; // Muestra "MENU" cuando está cerrado
+    }
+}
+
+
+
